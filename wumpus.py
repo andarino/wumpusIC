939e4363476b95c84a2330b85f411ba898752fa0
andarino <tj.vinci97@gmail.com>
2021-03-03 23:30:58 -0300

    Ambiente do jogo (em desenvolvimento)

---
+++ wumpus.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+def imprime(l, c, matriz):
+    for i in range(l):
+        for j in range(c):
+            if(j == c - 1):
+                print("[%s]\n" %matriz[i][j], end = "")
+            else:
+                print("[%s]" %matriz[i][j], end = "")
+                print(" ", end = "")
+
+def crie_matriz(n_linhas, n_colunas, valor):
+    matriz = [] # lista vazia
+    for i in range(n_linhas):
+        # cria a linha i
+        linha = [] # lista vazia
+        for j in range(n_colunas):
+            linha += [valor]
+
+        # coloque linha na matriz
+        matriz += [linha]
+
+    return matriz
+
+def c_cantos(l, c):
+    se = [0, 0]  #superior superior esquerdo
+    sd = [0, c]  #superior direito
+    ie = [l, 0]  #inferior esquerdo
+    id = [l, c]  #inferior direito
+
+#-----------------------
+
+def main():
+    A = crie_matriz(5,5,0)
+    A[1][1] = 'x'
+    A[1][0] = 'x'
+    A[1][2] = 'x'
+    A[1][3] = 'x'
+    A[1][4] = 'x'
+    imprime(5,5,A)
+
+if __name__ == '__main__':
+        main()
